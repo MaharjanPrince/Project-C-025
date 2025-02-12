5a5e680ef84a6cbfe020c35a470ca871275c92e9
Nirad Ojha <ojhanirad@gmail.com>
2025-02-12 07:16:06 +0545

    something

--- maze.c
+++ maze.c
@@ -1,5 +1,18 @@
 // Input and output for the maize
 #include<stdio.h>
+#define ROW 6
+#define COLUMN 6
+int maize[ROW][COLUMN]={
+    {'S', '0', '1', '1', '1', '1'},
+    {'1', '0', '1', '0', '0', '0'},
+    {'1', '0', '1', '0', '1', 'E'},
+    {'1', '0', '0', '0', '1', '1'},
+    {'1', '1', '1', '1', '1', '1'}
+};
+void maizeprint(int,int);
 void main(){
-    printf("hello world");
+    maizeprint(ROW,COLUMN);
 }
+void maizeprint(ROW,COLUMN){
+    int i;
+    for(i=
