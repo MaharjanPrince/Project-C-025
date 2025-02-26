9de4d599a75b13428a89c9abaadf9befac1bd556
Nirad Ojha <ojhanirad@gmail.com>
2025-02-26 12:30:00 +0545

    imput for the maize

--- maze.c
+++ maze.c
@@ -1,18 +1,15 @@
-// Input and output for the maize
-#include<stdio.h>
-#define ROW 6
-#define COLUMN 6
-int maize[ROW][COLUMN]={
-    {'S', '0', '1', '1', '1', '1'},
-    {'1', '0', '1', '0', '0', '0'},
-    {'1', '0', '1', '0', '1', 'E'},
-    {'1', '0', '0', '0', '1', '1'},
-    {'1', '1', '1', '1', '1', '1'}
-};
-void maizeprint(int,int);
-void main(){
-    maizeprint(ROW,COLUMN);
+//Function to read the maze from user input
+void readMaze()
+{
+    int i,j,rows,cols,maze[100];
+    printf("Enter the number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    printf("Enter the maze (%d x %d) using 0 for path, 1 for wall, S for start, E for end:\n", rows, cols);
+    for (i = 0; i < rows; i++)
+    {
+        for(j = 0; j < cols; j++){
+        scanf("%s", maze[i]);
+        }
+    }
 }
-void maizeprint(ROW,COLUMN){
-    int i;
-    for(i=
