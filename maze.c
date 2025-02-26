aff5f2f6abd3a45a13c4342d5d619a67e512be93
Nirad Ojha <ojhanirad@gmail.com>
2025-02-26 13:22:32 +0545

    input for the maze

--- maze.c
+++ maze.c
@@ -1,15 +1,13 @@
 //Function to read the maze from user input
-void readMaze()
+void readmaze()
 {
-    int i,j,rows,cols,maze[100];
+   
     printf("Enter the number of rows and columns: ");
     scanf("%d %d", &rows, &cols);
 
     printf("Enter the maze (%d x %d) using 0 for path, 1 for wall, S for start, E for end:\n", rows, cols);
-    for (i = 0; i < rows; i++)
+    for (int i = 0; i < rows; i++)
     {
-        for(j = 0; j < cols; j++){
         scanf("%s", maze[i]);
         }
-    }
 }
